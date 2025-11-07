507232f5cec4365ebc114c5011a62fb711876742
Tomas Di Leo <tomasjdle@gmail.com>
2025-11-06 23:50:15 -0300

    Cambio de nombre a uno mas representativo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -17,11 +17,11 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+#include <system.h>
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "states.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
